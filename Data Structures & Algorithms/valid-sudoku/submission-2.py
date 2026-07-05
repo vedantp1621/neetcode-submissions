8d4f6bae2fb654745ed9e25003835a5e03c2c5c7
Vedant Patel <112649299+vedantp1621@users.noreply.github.com>
2026-07-05 00:02:49 -0500

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set) # holds sets of all seen nums within a given col
+        rows = defaultdict(set) # holds sets of all seen nums within a given row
+        squares = defaultdict(set)
+
+        
+        for r in range(9): # row by row   
+            for c in range(9): # col by col
+                
+                if board[r][c] == ".":
+                    continue
+
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+
+        return True
+
+
+
+
+
+        
