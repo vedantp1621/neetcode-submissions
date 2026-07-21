561f7e53b9dc25a7765659cd6d60c3e7e9203928
Vedant Patel <112649299+vedantp1621@users.noreply.github.com>
2026-07-20 19:34:49 -0500

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        print(heights) #yes
+        # must leave unsorted
+        # track max seen height
+        # only move the 'limiting' tower left or right
+
+        left, right = 0, len(heights)-1
+        max_height = 0
+
+        while left < right:
+            box = (right - left) * min(heights[left], heights[right])
+
+            if box > max_height:
+                max_height = box
+
+            if heights[left] <= heights[right]:
+                left += 1
+                continue
+
+            if heights[left] > heights[right]:
+                right -= 1
+                continue
+
+        return max_height
+
+            
+        
